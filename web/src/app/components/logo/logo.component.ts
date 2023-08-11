420880b4866c1ed385265bae65ab6e4091f1a6cf
Adiel Pereira <adielp16@gmail.com>
2023-08-10 21:32:28 -0300

    fix: update logo in footer component

--- web/src/app/components/logo/logo.component.ts
+++ web/src/app/components/logo/logo.component.ts
@@ -1,3 +1,4 @@
+import { DomSanitizer } from '@angular/platform-browser';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -8,4 +9,18 @@ import { Component, Input } from '@angular/core';
 export class LogoComponent {
   @Input() variant: 'default' | 'footer' = 'default';
   title = 'Paróquia São Benedito';
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
+
+  get brand() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.variant === 'default' ? 'assets/brand.svg' : 'assets/brand-white.svg'
+    );
+  }
+
+  get logo() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.variant === 'default' ? 'assets/logo.png' : 'assets/logo-stroke.png'
+    );
+  }
 }
