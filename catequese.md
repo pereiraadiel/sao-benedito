0a3fb314c0fa086236428bc8114b820a08a0d158
Adiel Pereira <adielp16@gmail.com>
2023-08-21 09:20:09 -0300

    feat: home page component images carousel

---
+++ catequese.md
@@ -0,0 +1,39 @@
+Creio em Deus Pai todo poderoso
+
+criador do céu e da terra
+
+e em Jesus Cristo seu Unico Filho
+
+nosso senhor
+
+que foi concebido pelo poder do Espirito Santo
+
+Nasceu da Virgem Maria
+
+padeceu sob poncio Pilatos
+
+foi crucificado morto e sepultado
+
+desceu a mansão dos mortos
+
+ressucitou ao terceiro dia
+
+subiu aos ceus
+
+esta sentado a direita de Deus Pai todo poderoso
+
+de onde há de vir a julgar os vivos e os mortos
+
+creio no Espirito Santo
+
+na Santa Igreja Católica
+
+na comunhão dos santos
+
+na remissão dos pecados
+
+na ressureição da carne
+
+na vida eterna,
+
+Amém!
--- web/src/app/pages/home/homePage.component.ts
+++ web/src/app/pages/home/homePage.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './homePage.component.html',
   styleUrls: ['./homePage.component.scss'],
 })
-export class HomePageComponent {
-  title = 'Paróquia São Benedito';
+export class HomePageComponent implements OnInit, OnDestroy {
+  @ViewChild('heroBackground') heroBackground: ElementRef | undefined;
+  private images: string[] = [
+    'https://files.adiel.dev/paroquia-saobenedito.webp',
+    'https://files.adiel.dev/missa.webp',
+    'https://files.adiel.dev/sb-altar.webp',
+    'https://files.adiel.dev/sb-nave-01.webp',
+    'https://files.adiel.dev/missa-02.webp',
+    'https://files.adiel.dev/sb-nossa-senhora.webp',
+  ];
+  private subscription: Subscription | undefined;
+  private currentImageIndex: number = 0;
+
+  constructor(
+    private renderer: Renderer2,
+    private readonly sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit() {
+    this.subscription = interval(7 * 1000).subscribe(() => {
+      this.changeHeroBackground();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  private changeHeroBackground() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+
+    const imageUrl = `url('${this.images[this.currentImageIndex]}')`;
+    this.renderer.setStyle(
+      this.heroBackground?.nativeElement,
+      'background-image',
+      imageUrl
+    );
+  }
+
+  get bgImage() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.images[this.currentImageIndex]
+    );
+  }
 }
