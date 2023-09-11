444764dc36bf295e3e130a8ccce0b2d4495d912f
Adiel Pereira <adielp16@gmail.com>
2023-09-11 10:02:24 -0300

    refact: modify hashUtil to use app auth secret

--- server/src/utils/hash.util.ts
+++ server/src/utils/hash.util.ts
@@ -1,11 +1,19 @@
 import * as crypto from 'crypto';
+import { AuthConstant } from '../constants/auth.constant';
 
 export const HashUtil = {
   hash: (value: string) => {
+    const secret = AuthConstant.jwt.secret;
+
+    const secretHash = crypto.createHmac('sha512', secret);
     const shahash = crypto.createHash('sha256');
     const md5hash = crypto.createHash('md5');
-    const stepOne = shahash.update(value).digest('hex');
+
+    const stepOne = secretHash
+      .update(shahash.update(value).digest('hex'))
+      .digest('hex');
     const stepTwo = md5hash.update(stepOne).digest('hex');
+
     shahash.destroy();
     md5hash.destroy();
     return Buffer.from(stepTwo).toString('base64');
